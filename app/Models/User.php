9a3b4dda9d585c313e02df917b385eaa3d991e9a
Alemoh Rapheal <raphealenike@gmail.com>
2021-07-03 03:11:19 -0700

    Add columns

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,6 +10,14 @@ use Illuminate\Notifications\Notifiable;
 class User extends Authenticatable
 {
     use HasFactory, Notifiable;
+    
+
+    public function getAvatarUrl()
+    {
+        $url = $this->avatar_url;
+        return $url;
+    }
+
 
     /**
      * The attributes that are mass assignable.
